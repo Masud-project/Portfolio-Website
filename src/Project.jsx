5d9486e57672c9d21be0f8950248049bc6b86411
Masud-project <masud018793625@gmail.com>
2026-06-25 07:11:45 +0600

    Correct typo in Project component description

--- src/Project.jsx
+++ src/Project.jsx
@@ -1,7 +1,7 @@
 function Project () {
 return (
     <div className="Projects shadow-lg text-primary text-align-center p-3">
-        <h5> Every types of Websit I can make. Day by day I update my skills. Also, I have experience of using AI tools for looking attractive my website </h5>
+        <h5> Every types of Website I can make. Day by day I update my skills. Also, I have experience of using AI tools for looking attractive my website </h5>
     <p>I have made some project, My previous project link is 
         <span>
             <a className="text-success text-bold fs-5" href="https://nursery-project-2nd.vercel.app/">***NURSERY-PROJECT***</a>
